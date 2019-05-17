29e0259a1b3c6a197313f6d05ad86b4e332a0d69
Damien Villeneuve <d.villeneuve@beapp.fr>
2019-05-17 19:21:45 +0200

    Update script to build and push logzio-filebeat image

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-#version=0.1
+# You should login first by using : docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+version=0.1
 image_root_name="beappers"
 images=(
   php-7.1-fpm
   php-7.1-fpm-xdebug
   engine-7.1
+  logzio-filebeat
 )
 
-#docker login -u $DOCKER_USER -p $DOCKER_PASS
+function build() {
+  image=$1
+  version=$2
 
-for image in ${images[@]}; do
-  echo "Building image $image"
-
-  if [ ! -z $version ]; then
+  if [[ ! -z ${version} ]]; then
+    echo "Building image $image with version $version"
     docker build -t "$image_root_name/$image:$version" "$image"
     docker tag "$image_root_name/$image:$version" "$image_root_name/$image:latest"
   else
+    echo "Building image $image"
     docker build -t "$image_root_name/$image:latest" "$image"
   fi
+}
+
+function upload() {
+  image=$1
+  version=$2
 
   echo "Uploading image $image"
-  if [ ! -z $version ]; then
+  if [[ ! -z ${version} ]]; then
     docker push "$image_root_name/$image:$version"
   fi
   docker push "$image_root_name/$image:latest"
+}
+
+for image in ${images[@]}; do
+  build "$image" "$version"
+  upload "$image" "$version"
 done
